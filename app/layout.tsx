8b7635e6055ed8793687fb85e99f26bb78afe27b
Julian S. <200318@studierende.htl-donaustadt.at>
2024-05-30 16:33:16 +0200

    chore: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const fontSans = FontSans({
   variable: "--font-sans",
 });
 export const metadata: Metadata = {
-  title: "Magic UI",
-  description: "The startup template from Magic UI",
+  title: "Hebbosites",
+  description: "Driving Website Success & Growth",
 };
 
 export default function RootLayout({
